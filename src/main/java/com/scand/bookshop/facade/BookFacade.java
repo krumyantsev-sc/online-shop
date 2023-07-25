8b7c8dc67a25445930c45691e28f4c3763e669e2
krumyantsev-sc <rumyantsev@scand.com>
2023-07-25 09:54:24 +0300

    fix: book order after updating

--- src/main/java/com/scand/bookshop/facade/BookFacade.java
+++ src/main/java/com/scand/bookshop/facade/BookFacade.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -51,7 +52,7 @@ public class BookFacade {
 
     public PageResponseDTO getBooksPage(int pageNumber, int pageSize) {
         System.out.println(pageNumber + pageSize);
-        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("id"));
         Page<Book> bookPage = bookService.getAllBooks(pageable);
         int totalPages = bookPage.getTotalPages();
         return new PageResponseDTO(bookPage.map(DTOConverter::toDTO).getContent(), totalPages);
--- src/main/java/com/scand/bookshop/service/BookService.java
+++ src/main/java/com/scand/bookshop/service/BookService.java
@@ -5,6 +5,7 @@ import com.scand.bookshop.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
